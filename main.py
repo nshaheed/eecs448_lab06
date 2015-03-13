5954e6884ecdd6a46cd19640dd8894e3977afc33
Nicholas Shaheed <nshaheed@1005d-14.eecs.ku.edu>
2015-03-13 17:03:36 -0500

    got outline of file reading and checking done

--- main.py
+++ main.py
@@ -1,4 +1,38 @@
+# shamelessly copied from stackoverflow
+def checkEqual(iterator):
+    return len(set(iterator)) <= 1
+
 file = open("matrix1.csv","r")
 
+matStr = []
+matFlo = []
+counter = 0;
+
+# print matStr
+
+# read file
 for line in file:
-    
+    matStr.insert(counter, line.split(','))
+    counter = counter + 1
+
+print matStr
+
+# convert contents of file to floats
+for i in range(0,len(matStr)):
+    temp = map(float, matStr[i])
+    matFlo.insert(i, temp)
+
+print matFlo
+
+# check if this is a proper matrix
+length = map(len, matFlo)
+print length
+
+if (checkEqual(length)):
+    # do stuff because it's a matrix
+    print("it worked")
+else:
+    # it wasn't a matrix
+    print("That file is not a matrix\n")
+
+
