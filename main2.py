91cd6292db7c37703da16e2d5d66064703e9c441
Lucas Dzin Pedroso <ldzinped@1005-23.eecs.ku.edu>
2015-03-31 12:54:38 -0500

    main2.py

--- main2.py
+++ main2.py
@@ -1,13 +1,12 @@
 #multiplication
 def mult(matrix1, matrix2):
 	if len(matrix1[1]) == len(matrix2):
-		dimen1 = len(matrix2[1]) #columns matrix2
-		dimen2 = len(matrix1) #rows matrix1
-		matrixresult = [ [ 0 for i in range(dimen1) ] for j in range(dimen2) ]
-		for x in range(0, dimen1):
-			for y in range(0, dimen2):
+		print "Multiplication:"
+		matrixresult = [[0 for i in range(len(matrix2[1]))] for j in range(len(matrix1))]
+		for x in range(0, len(matrix1)):
+			for y in range(0, len(matrix2[1])):
 				matrixresult[x][y] = 0
-				for z in range(0, dimen1):
+				for z in range(0, len(matrix2)):
 					matrixresult[x][y] = matrixresult[x][y] + (matrix1[x][z] * matrix2[z][y])
 		return matrixresult
 	else:
@@ -16,11 +15,10 @@ def mult(matrix1, matrix2):
 #addition
 def add(matrix1, matrix2):
 	if len(matrix1) == len(matrix2) and len(matrix1[1]) == len(matrix2[1]):
-		dimen1 = len(matrix1) #rows matrix1 and matrix2
-		dimen2 = len(matrix1[1]) #columns matrix1 and matrix2
-		matrixresult = [ [ 0 for i in range(dimen1) ] for j in range(dimen2) ]
-		for x in range(0, dimen1):
-			for y in range(0, dimen2):
+		print "Addition:"
+		matrixresult = [[0 for i in range(len(matrix1[1])) ] for j in range(len(matrix1))]
+		for x in range(0, len(matrix1)):
+			for y in range(0, len(matrix1[1])):
 				matrixresult[x][y] = matrix1[x][y] + matrix2[x][y]
 		return matrixresult
 	else:
@@ -28,20 +26,21 @@ def add(matrix1, matrix2):
 
 #transpose
 def trans(matrix):
-	dimen1 = len(matrix) #rows matrix
-	dimen2 = len(matrix[1]) #columns matrix
-	matrixresult = [ [ 0 for i in range(dimen1) ] for j in range(dimen2) ]
-	for x	in range(0, dimen1):
-		for y in range(0, dimen2):
+	print "Transpose:"
+	matrixresult = [[0 for i in range(len(matrix)) ] for j in range(len(matrix[1]))]
+	for x in range(0, len(matrix[1])):
+		for y in range(0, len(matrix)):
 			matrixresult[x][y] = matrix[y][x]
 	return matrixresult	
 	
-	
-Matrix = [[1,2],[3,4]]
-Matrix1 = [[1,2],[3,4]]
+#test
+Matrix1 = [[1,2,3],[4,5,6]]
+Matrix2 = [[1,2,3,4],[5,6,7,8],[9,10,11,12]]
+Matrix3 = [[1,2,3],[4,5,6],[7,8,9]]
+Matrix4 = [[1,2,3],[4,5,6]]
 
-print mult(Matrix, Matrix1)
+print mult(Matrix1, Matrix2)
 
-print add(Matrix, Matrix1)
+print add(Matrix3, Matrix3)
 
-print trans(Matrix)
+print trans(Matrix4)
